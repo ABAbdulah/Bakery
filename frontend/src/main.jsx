2f3543a04bb73a7aedf2b5834ac66c4ebc8e8c4a
Abdullah Tahir <ababdullah216@gmail.com>
2024-06-12 22:31:52 +0500

    DivLynx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import ContactForm from "./component/ContactUs.js";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    // <ContactForm />
+    <App />
   </React.StrictMode>
 );
